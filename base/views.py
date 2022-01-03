b115a22c9432db8173e1b6cbf33d5f82b99bcb31
ExplorerKut <kutbuddin.ambawala@gmail.com>
2022-01-03 16:30:34 +0530

    Initial Commit

---
+++ base/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Room
+from .forms import RoomForm
+# rooms=[
+#     {'id':1, 'name':'Lets Learn Python'},
+#     {'id':2, 'name':'Lets Learn Java'},
+#     {'id':3, 'name':'Lets Learn Go'},
+#     {'id':4, 'name':' Lets Learn .Net'}
+
+# ]
+def home(request):
+    rooms= Room.objects.all()
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room=Room.objects.get(id=pk)
+    context={'room':room}
+    return render(request,'base/room.html',context)
+
+
+def createRoom(request):
+    form=RoomForm()
+    if request.method=='POST':
+        form= RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
+
+def updateRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    form=RoomForm(instance=room)
+    
+    if request.method=='POST':
+        form=RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save
+            return redirect("home")
+
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
